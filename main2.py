5ab1076cc01fd0e73ac84b401c008d4299b49a17
Adel <adi.stein@outlook.cz>
2024-05-07 15:48:34 +0200

    dddd

--- main2.py
+++ main2.py
@@ -1,3 +1,10 @@
+import cv2
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+
+
 """
 bg_subtractor = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=50, detectShadows=False)
 
@@ -32,7 +39,7 @@ while True:
 cap.release()
 cv2.destroyAllWindows()
 """
-
+"""
 import cv2
 import numpy as np
 import tensorflow as tf
@@ -53,7 +60,7 @@ def recognizer():
 
             cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
             face = frame[y:y+h, x:x+w]
-            face = cv2.resize(face,(400,200))
+            face = cv2.resize(face,(200,200))
             face = face / 255.0
             face = np.expand_dims(face, axis=0)
 
@@ -69,3 +76,65 @@ def recognizer():
     cv2.destroyAllWindows()
 
 recognizer()
+"""
+
+
+"""
+
+import cv2
+import numpy as np
+
+# Načtení obrázku
+image = cv2.imread("9.jpg")
+
+# Převod obrázku do barevného prostoru HSV
+hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+# Určení rozsahu barev pro růžovou barvu
+lower_pink = np.array([120, 80, 80])  # Dolní mez pro odstín, sytost a hodnotu
+upper_pink = np.array([200, 255, 255])  # Horní mez pro odstín, sytost a hodnotu
+
+# Vytvoření masky pro růžovou barvu
+mask = cv2.inRange(hsv_image, lower_pink, upper_pink)
+
+# Aplikace masky na původní obrázek
+pink_objects = cv2.bitwise_and(image, image, mask=mask)
+
+# Zobrazení původního obrázku a detekovaných objektů s růžovou barvou
+cv2.imshow("Original Image", image)
+cv2.imshow("Pink Objects", pink_objects)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+"""
+
+
+
+import cv2
+
+image = cv2.imread("9.jpg")
+image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+window_size = (50, 50)
+step_size = 10
+
+target_color = (255, 0, 0)
+
+color_tolerance = 50
+lower_dark_blue = np.array([90, 50, 50])
+upper_dark_blue = np.array([120, 255, 255])
+
+dark_blue_mask = cv2.inRange(image_rgb, lower_dark_blue, upper_dark_blue)
+
+# Aplikace masky na obrázek
+dark_blue_image = cv2.bitwise_and(image, image, mask=dark_blue_mask)
+cv2.imshow("Sliding Window", cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+
+
+
