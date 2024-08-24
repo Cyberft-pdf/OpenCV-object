2f01e632dea44a3cf7b3a5989721a2261bb43188
Adel <adi.stein@outlook.cz>
2024-08-24 16:13:22 +0200

    second check
    Now it is ready for public

--- color_subtraction.py
+++ color_subtraction.py
@@ -6,6 +6,12 @@ import matplotlib.pyplot as plt
 
 
 """
+-----------------------------------------------------------------------------------------------------------------------
+
+    This code snippet uses OpenCV to perform background subtraction and contour detection on a video feed from a webcam
+
+-----------------------------------------------------------------------------------------------------------------------    
+
 bg_subtractor = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=50, detectShadows=False)
 
 
@@ -20,11 +26,7 @@ while True:
 
     fg_mask = bg_subtractor.apply(frame)
     
-    #nalezaní kontur
     contours, _ = cv2.findContours(fg_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    
-
 
 
 
@@ -39,79 +41,45 @@ while True:
 cap.release()
 cv2.destroyAllWindows()
 """
-"""
-import cv2
-import numpy as np
-import tensorflow as tf
 
-
-def recognizer():
-
-    cap = cv2.VideoCapture(0)
-
-    liver_cascade = cv2.CascadeClassifier("/Users/Adela/Desktop/data/classifier/cascade.xml")
-
-    while True:
-        ret, frame = cap.read()  
-
-        liver = liver_cascade.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40))
-
-        for (x, y, w, h) in liver:
-
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-            face = frame[y:y+h, x:x+w]
-            face = cv2.resize(face,(200,200))
-            face = face / 255.0
-            face = np.expand_dims(face, axis=0)
-
-
-        cv2.imshow("Live Detekce", frame)
-
-
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
-
-
-    cap.release()
-    cv2.destroyAllWindows()
-
-recognizer()
-"""
 
 
 """
+----------------------------------------------------------------
 
-import cv2
-import numpy as np
 
-# Načtení obrázku
+This code snippet demonstrates how to detect pink objects
+
+---------------------------------------------------------------- 
+
+
 image = cv2.imread("9.jpg")
 
-# Převod obrázku do barevného prostoru HSV
 hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-# Určení rozsahu barev pro růžovou barvu
-lower_pink = np.array([120, 80, 80])  # Dolní mez pro odstín, sytost a hodnotu
-upper_pink = np.array([200, 255, 255])  # Horní mez pro odstín, sytost a hodnotu
+lower_pink = np.array([120, 80, 80]) 
+upper_pink = np.array([200, 255, 255])  
 
-# Vytvoření masky pro růžovou barvu
 mask = cv2.inRange(hsv_image, lower_pink, upper_pink)
 
-# Aplikace masky na původní obrázek
 pink_objects = cv2.bitwise_and(image, image, mask=mask)
 
-# Zobrazení původního obrázku a detekovaných objektů s růžovou barvou
 cv2.imshow("Original Image", image)
 cv2.imshow("Pink Objects", pink_objects)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
 """
 
 
 
-import cv2
+
+"""
+---------------------------------------------------------------------------------------------------------------------------------------------------------------    
+
+This code snippet applies a sliding window approach to detect areas of an image that match a certain color range, specifically targeting dark blue regions
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------    
+
 
 image = cv2.imread("9.jpg")
 image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -134,7 +102,7 @@ cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 
-
+"""
 
 
 
